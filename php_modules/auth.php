70452741a9c2376517ccf27745103736ffbef13c
Jan Huthmann <mr@devjay.org>
2015-11-02 22:20:27 +0100

    formatting, clean-up

--- php_modules/auth.php
+++ php_modules/auth.php
@@ -2,13 +2,13 @@
 	include_once 'db_connection.php';
 	include_once 'db_session.php';     
 
-    new Session($mysqli);
+	new Session($mysqli);
    
-    $hostname = $_SERVER['HTTP_HOST'];
-    $path = dirname($_SERVER['PHP_SELF']);
+	$hostname = $_SERVER['HTTP_HOST'];
+	$path = dirname($_SERVER['PHP_SELF']);
 
-    if (!isset($_SESSION['registered']) || !$_SESSION['registered']) {
-    	header('Location: http://'.$hostname.($path == '/' ? '' : $path).'/login.php');
-     	exit;
-    }
+	if (!isset($_SESSION['registered']) || !$_SESSION['registered']) {
+		header('Location: http://'.$hostname.($path == '/' ? '' : $path).'/login.php');
+		exit;
+	}
 ?>
