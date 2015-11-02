a7d56d4bf6567ff9b6bf3f4f3c4edd0ed9fa68cf
Jan Huthmann <mr@devjay.org>
2015-11-02 22:00:32 +0100

    removed root password

--- php_modules/db_config.php
+++ php_modules/db_config.php
@@ -2,7 +2,7 @@
 
 DEFINE("MYSQL_HOST","localhost");
 DEFINE("MYSQL_USER", "root");
-DEFINE("MYSQL_PASSWORD", "root");
+DEFINE("MYSQL_PASSWORD", "");
 DEFINE("MYSQL_DATABASE", "phpdemo");
 
 DEFINE("MYSQL_TABLE_USERS", "users");
